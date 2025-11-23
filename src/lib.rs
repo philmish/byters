53de7d888a68442a6357e51bca95e7ab4981a40a
philmish <philmish.dev@gmail.com>
2025-11-23 17:15:23 +0100

    refactor: make reading and setting bits type safe
    Refactor bit setting and reading
    
    - refactor HiBit to Bits trait, providing MAX_BIT_OFFSET and MAX_MASK as
    const values
    - enforce valid positions for single bit and bit ranges through types
    - refactor impls and tests to use new types for reading and setting bits

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use private::Sealed;
 
 mod private {
@@ -25,21 +27,116 @@ impl Byte for u8 {
     }
 }
 
-/// read the bit at `pos` as a single byte
-/// positions are treated as 0-based indices,
-/// treating the least significant bit as the
-/// first index
-pub trait ReadableBit: HiBit + private::Sealed {
-    fn read_bit(&self, pos: usize) -> u8;
+/// defines the maximum bit offset for a type which
+/// can be read as a series of bits.
+/// this trait can not be implemented by any other type.
+pub trait Bits: Sized + private::Sealed {
+    /// max. offset from the least significant bit
+    const MAX_BIT_OFFSET: usize;
+    const MAX_MASK: Self;
+}
+
+macro_rules! impl_bits {
+    ($t:ty,$o:expr) => {
+        impl Bits for $t {
+            const MAX_BIT_OFFSET: usize = $o;
+            const MAX_MASK: $t = Self::MAX;
+        }
+    };
+}
+
+impl_bits!(u8, 7);
+impl_bits!(u16, 15);
+impl_bits!(u32, 31);
+impl_bits!(u64, 63);
+
+#[derive(Debug)]
+pub enum BytersError {
+    BitPositionBound { given: usize, max: usize },
+    BitRangeInvalid { start: usize, end: usize },
+}
+
+/// generic `Result` type for the `byters` crate
+pub type BytersResult<T> = Result<T, BytersError>;
+
+/// the position of a bit in a type T which implements
+/// the `HiBit` trait.
+/// Ensures position validity on initialization.
+pub struct BitPos<T: Bits> {
+    pub(crate) pos: usize,
+    phantomdata: PhantomData<T>,
+}
+
+impl<T> BitPos<T>
+where
+    T: Bits,
+{
+    /// validates that the given bit offset `p` is inside
+    /// the bounds of the bits of Type `T`.
+    /// Returns a `BitPositionBound` error if the passed
+    /// `p` exceeds the max. bit offset for Type `T`.
+    pub fn new(p: usize) -> BytersResult<BitPos<T>> {
+        if p > T::MAX_BIT_OFFSET {
+            return Err(BytersError::BitPositionBound {
+                given: p,
+                max: T::MAX_BIT_OFFSET,
+            });
+        }
+        return Ok(Self {
+            pos: p,
+            phantomdata: PhantomData,
+        });
+    }
+}
+
+pub struct BitRange<T: Bits> {
+    pub(crate) start: usize,
+    pub(crate) end: usize,
+    phantomdata: PhantomData<T>,
+}
+
+impl<T> BitRange<T>
+where
+    T: Bits,
+{
+    pub fn new(start: usize, end: usize) -> BytersResult<BitRange<T>> {
+        if start >= end {
+            return Err(BytersError::BitRangeInvalid { start, end });
+        }
+        if end > T::MAX_BIT_OFFSET {
+            return Err(BytersError::BitPositionBound {
+                given: end,
+                max: T::MAX_BIT_OFFSET,
+            });
+        }
+        Ok(BitRange {
+            start,
+            end,
+            phantomdata: PhantomData,
+        })
+    }
+
+    pub(crate) fn mask_lshift(&self) -> usize {
+        T::MAX_BIT_OFFSET - self.end
+    }
+
+    pub(crate) fn mask_rshift(&self) -> usize {
+        self.mask_lshift() + self.start
+    }
+}
+
+/// a type which implement `Bits` can read a bit
+/// at position `pos`.
+/// the validity of the position is enforced on the type level
+pub trait ReadableBit: Bits {
+    fn read_bit(&self, pos: BitPos<Self>) -> u8;
 }
 
 macro_rules! impl_readable_bit {
     ($s:ty) => {
         impl ReadableBit for $s {
-            fn read_bit(&self, pos: usize) -> u8 {
-                let last_bit = Self::hi_bit_idx();
-                assert!(last_bit >= pos);
-                ((self >> pos) & 1) as u8
+            fn read_bit(&self, pos: BitPos<$s>) -> u8 {
+                ((self >> pos.pos) & 1) as u8
             }
         }
     };
@@ -50,16 +147,15 @@ impl_readable_bit!(u16);
 impl_readable_bit!(u32);
 impl_readable_bit!(u64);
 
-pub trait SetableBit: HiBit + private::Sealed {
-    fn set_bit(&mut self, pos: usize);
+pub trait SetableBit: Bits {
+    fn set_bit(&mut self, pos: BitPos<Self>);
 }
 
 macro_rules! impl_setable_bit {
     ($s:ty) => {
         impl SetableBit for $s {
-            fn set_bit(&mut self, pos: usize) {
-                assert!(Self::hi_bit_idx() >= pos);
-                *self |= (1 << pos);
+            fn set_bit(&mut self, pos: BitPos<$s>) {
+                *self |= (1 << pos.pos);
             }
         }
     };
@@ -70,20 +166,16 @@ impl_setable_bit!(u16);
 impl_setable_bit!(u32);
 impl_setable_bit!(u64);
 
-pub trait SetableBits: HiBit + private::Sealed {
-    fn set_bits(&mut self, start: usize, end: usize);
+pub trait SetableBits: Bits {
+    fn set_bits(&mut self, range: BitRange<Self>);
 }
 
 macro_rules! impl_setable_bits {
     ($s:ty) => {
         impl SetableBits for $s {
-            fn set_bits(&mut self, start: usize, end: usize) {
-                let hi_bit_idx = Self::hi_bit_idx();
-                assert!(start < end, "start cant be greater or equal to end");
-                assert!(end <= hi_bit_idx, "end is out of bounds");
-                let m_l = hi_bit_idx - end;
-                let m_r = m_l + start;
-                let mask = ((<$s>::MAX << m_l) >> m_r) << start;
+            fn set_bits(&mut self, range: BitRange<$s>) {
+                let mask =
+                    ((<$s>::MAX_MASK << range.mask_lshift()) >> range.mask_rshift()) << range.start;
                 *self |= mask;
             }
         }
@@ -96,20 +188,15 @@ impl_setable_bits!(u16);
 /// read a range of bits starting from bit index `start` up to and including
 /// `end` returning it as a `T`.
 /// `start` and `end` are treated as 0-based indices
-pub trait BitsReadableAs<T>: HiBit + private::Sealed {
-    fn read_bits_as(&self, start: usize, end: usize) -> T;
+pub trait BitsReadableAs<T>: Bits {
+    fn read_bits_as(&self, range: BitRange<Self>) -> T;
 }
 
 macro_rules! impl_bits_readable_as {
     ($s:ty,$o:ty) => {
         impl BitsReadableAs<$o> for $s {
-            fn read_bits_as(&self, start: usize, end: usize) -> $o {
-                assert!(start < end);
-                let hi_bit_idx = Self::hi_bit_idx();
-                assert!(end <= hi_bit_idx);
-                let r = hi_bit_idx - end;
-                let l = r + start;
-                ((self << r) >> l) as $o
+            fn read_bits_as(&self, range: BitRange<$s>) -> $o {
+                ((self << range.mask_lshift()) >> range.mask_rshift()) as $o
             }
         }
     };
@@ -118,26 +205,6 @@ macro_rules! impl_bits_readable_as {
 impl_bits_readable_as!(u8, u8);
 impl_bits_readable_as!(u8, u16);
 impl_bits_readable_as!(u16, u8);
-
-/// defines the highest bit index for a type.
-pub trait HiBit {
-    fn hi_bit_idx() -> usize;
-}
-
-macro_rules! impl_hi_bit {
-    ($t:ty,$o:expr) => {
-        impl HiBit for $t {
-            fn hi_bit_idx() -> usize {
-                $o
-            }
-        }
-    };
-}
-
-impl_hi_bit!(u8, 7);
-impl_hi_bit!(u16, 15);
-impl_hi_bit!(u32, 31);
-impl_hi_bit!(u64, 63);
 
 /// unified API for reading data in big endian
 /// order
@@ -226,26 +293,31 @@ impl ReadsIntoBytes for LittleEndian {
 #[cfg(test)]
 mod tests {
     use crate::{
-        BigEndian, BitsReadableAs, LittleEndian, ReadableBit, ReadsFromBytes, ReadsIntoBytes,
-        SetableBit, SetableBits,
+        BigEndian, BitPos, BitRange, BitsReadableAs, LittleEndian, ReadableBit, ReadsFromBytes,
+        ReadsIntoBytes, SetableBit, SetableBits,
     };
 
     #[test]
     fn bits_readable() {
         let val_u8: u8 = 0b1010_1010;
-        let out: u8 = val_u8.read_bits_as(1, 5);
+        let r = BitRange::<u8>::new(1, 5).unwrap();
+        let out: u8 = val_u8.read_bits_as(r);
         assert_eq!(out, 0b0010101);
     }
 
     #[test]
     fn bit_readable() {
         let val_u8: u8 = 0b0001_0000;
-        assert_eq!(val_u8.read_bit(4), 1);
-        assert_eq!(val_u8.read_bit(3), 0);
+        let pos_4_u8 = BitPos::<u8>::new(4).unwrap();
+        let pos_3_u8 = BitPos::<u8>::new(3).unwrap();
+        assert_eq!(val_u8.read_bit(pos_4_u8), 1);
+        assert_eq!(val_u8.read_bit(pos_3_u8), 0);
+        let pos_8_u16 = BitPos::<u16>::new(8).unwrap();
         let val_u16: u16 = 0x0100;
-        assert_eq!(val_u16.read_bit(8), 1);
+        assert_eq!(val_u16.read_bit(pos_8_u16), 1);
+        let pos_16_u32 = BitPos::<u32>::new(16).unwrap();
         let val_u32: u32 = 0x00010000;
-        assert_eq!(val_u32.read_bit(16), 1);
+        assert_eq!(val_u32.read_bit(pos_16_u32), 1);
     }
 
     #[test]
@@ -292,15 +364,17 @@ mod tests {
 
     #[test]
     fn set_bit() {
+        let pos_2_u8 = BitPos::<u8>::new(2).unwrap();
         let mut val_u8: u8 = 0;
-        val_u8.set_bit(2);
+        val_u8.set_bit(pos_2_u8);
         assert_eq!(val_u8, 4);
     }
 
     #[test]
     fn set_bits() {
         let mut val_u8 = 0u8;
-        val_u8.set_bits(1, 3);
+        let r = BitRange::<u8>::new(1, 3).unwrap();
+        val_u8.set_bits(r);
         assert_eq!(val_u8, 14);
     }
 }
